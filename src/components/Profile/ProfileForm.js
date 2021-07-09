8dae3d0a3294b1d307f3228aba1b24dade7d3f65
Andrews <allwinandrews@gmail.com>
2021-07-09 18:10:02 +0530

    Redirects to '/' on Login & Password Change

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,12 @@
 import { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import AuthContext from "../../store/auth-context";
 import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
+
   const newPasswordInputRef = useRef();
 
   const authCtx = useContext(AuthContext);
@@ -31,6 +34,7 @@ const ProfileForm = () => {
       // Assumption: Always Succeeds
 
       console.log(res);
+      history.replace("/");
     });
   };
 
